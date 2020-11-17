d1a76d0ccbb0cdd9d8cab4653ee09cb7720366e6
Lindsay Kinn <lindsay.kinn@gmail.com>
2020-11-17 10:00:06 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+
+def name  #reads the name
+    @name
+end
+
+def name=(name)  #writes the name
+    @name = name
+
+end
+
+def breed
+    @breed=breed
+end
+
+def breed
+    @breed
+end
+
+def breed=(breed)
+    @breed=breed
+end
+
+end
+
+
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person
+
+    def name  #reads the name
+        @name
+    end
+    
+    def name=(name)  #writes the name
+        @name = name
+    
+    end
+
+    def job
+        @job
+    end
+
+    def job=(job)
+        @job=job
+    end
+
+end
+
